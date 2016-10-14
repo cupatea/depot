b28c838e5c254b7ff647b374d8dc788817359cb3
cupatea <vdavyden@gmail.com>
2016-10-14 16:48:58 +0300

    should_create_line_item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
-class Product < ApplicationRecord
+class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01, message:
     'incorrect, please enter a value at least greater then 0.01 or equal one'}
@@ -11,5 +13,15 @@ class Product < ApplicationRecord
 
   def self.latest
     Product.order(:updated_at).last
+  end
+  private
+ # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
   end
 end
